424c10c48570a368c6cf2d7cb6ad4dd7fde7aba9
GATTAL Hamza <hmzgtl16@gmail.com>
2025-11-30 19:31:36 +0100

    feat: Add Android Library Compose convention plugin
    This commit introduces a new Gradle convention plugin, `hpoke.android.library.compose`, designed for Android library modules that use Jetpack Compose.
    
    The plugin applies the standard Android library and Kotlin Compose plugins and then uses the `configureAndroidCompose` extension function to set up the necessary Compose configurations.

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
@@ -0,0 +1,21 @@
+import com.android.build.gradle.LibraryExtension
+import com.example.hpoke.configureAndroidCompose
+import com.example.hpoke.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+
+class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = libs.findPlugin("com-android-library").get().get().pluginId)
+            apply(plugin = libs.findPlugin("org-jetbrains-kotlin-plugin-compose").get().get().pluginId)
+
+            extensions.configure<LibraryExtension>() {
+                configureAndroidCompose(this)
+            }
+        }
+    }
+}
