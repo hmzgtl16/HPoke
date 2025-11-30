41743682dea148f1dac901900938dd0eee9b5228
GATTAL Hamza <hmzgtl16@gmail.com>
2025-11-30 19:29:45 +0100

    feat: Introduce Android Library convention plugin
    This commit introduces a new Gradle convention plugin, `hpoke.android.library`, for configuring Android library modules.
    
    Key changes:
    - Adds the `AndroidLibraryConventionPlugin.kt` to define standard library configurations, such as the Kotlin Android setup, test runner, and resource prefix generation.
    - Adds `kotlin-test` as a default `testImplementation` and `androidTestImplementation` dependency for library modules.
    - Updates `gradle/libs.versions.toml` to include the `com.android.library` plugin and `kotlin-test` dependency.
    - Registers the new convention plugin in `build-logic/convention/build.gradle.kts`.

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,39 @@
+
+import com.android.build.gradle.LibraryExtension
+import com.example.hpoke.configureKotlinAndroid
+import com.example.hpoke.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = libs.findPlugin("com-android-library").get().get().pluginId)
+            apply(plugin = libs.findPlugin("org-jetbrains-kotlin-android").get().get().pluginId)
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 36
+                defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                testOptions.animationsDisabled = true
+                resourcePrefix =
+                    path.split("""\W""".toRegex())
+                        .drop(1)
+                        .distinct()
+                        .joinToString(separator = "_")
+                        .lowercase() + "_"
+            }
+            dependencies {
+                add(
+                    "androidTestImplementation",
+                    libs.findLibrary("org-jetbrains-kotlin-test").get()
+                )
+                add("testImplementation", libs.findLibrary("org-jetbrains-kotlin-test").get())
+            }
+        }
+    }
+}
