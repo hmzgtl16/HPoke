a01bd527b536b5b8c757c3e838aa937d1ec4054d
GATTAL Hamza <hmzgtl16@gmail.com>
2025-12-06 18:55:59 +0100

    feat: Add `feature:home` module
    This commit introduces the new `:feature:home` Gradle module.
    
    Key changes:
    - Adds the module to the project in `settings.gradle.kts`.
    - Includes a `build.gradle.kts` file that applies the `hpoke.android.feature` and `hpoke.android.library.compose` convention plugins.
    - Defines the namespace for the new module as `com.example.hpoke.feature.home`.
    - Adds a `.gitignore` file to exclude the `/build` directory.
    - Includes a basic `AndroidManifest.xml` for the module.

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.hpoke.android.feature)
+    alias(libs.plugins.hpoke.android.library.compose)
+}
+
+android {
+    namespace = "com.example.hpoke.feature.home"
+}
+
+dependencies {
+
+}
