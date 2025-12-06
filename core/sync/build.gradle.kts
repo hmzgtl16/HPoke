759ed41410df0bb25e6cdba278b96ad19010085e
GATTAL Hamza <hmzgtl16@gmail.com>
2025-12-06 17:20:38 +0100

    feat: Add Koin WorkManager dependency to sync module
    This commit integrates Koin with WorkManager in the `core:sync` module.
    
    Key changes:
    - Adds the `koin-androidx-workmanager` dependency to the version catalog (`libs.versions.toml`).
    - Implements `koin-core`, `koin-androidx-workmanager`, and `kotlinx-coroutines-core` in the `core:sync` module's dependencies.

--- core/sync/build.gradle.kts
+++ core/sync/build.gradle.kts
@@ -7,5 +7,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.org.jetbrains.kotlinx.coroutines.core)
+    implementation(libs.io.insert.koin.core)
+    implementation(libs.io.insert.koin.androidx.workmanager)
 }
