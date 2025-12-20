fb00a3ff4f3605b80e6bea07bba232f23ab71edb
GATTAL Hamza <hmzgtl16@gmail.com>
2025-12-20 15:56:34 +0100

    refactor: Remove `HomeUiState` sealed interface
    This commit removes the `HomeUiState` sealed interface from the `feature:home` module.
    
    The `HomeUiState` interface, along with its `Loading` and `Success` data classes, is no longer needed and has been deleted.

--- feature/home/src/main/kotlin/com/example/hpoke/feature/home/HomeUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hpoke.feature.home
-
-import com.example.hpoke.core.model.Pokemon
-
-sealed interface HomeUiState {
-    data object Loading : HomeUiState
-    data class Success(val pokemons: List<Pokemon>) : HomeUiState
-}
