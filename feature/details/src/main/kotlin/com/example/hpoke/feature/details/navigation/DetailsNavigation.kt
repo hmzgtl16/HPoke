e1b03d5ea8490878a0f6c07ca84ed922f893cc6f
GATTAL Hamza <hmzgtl16@gmail.com>
2025-12-20 18:49:26 +0100

    feat: Add navigation key for Details feature
    This commit introduces the navigation key for the `feature:details` module.
    
    A new data object `Details` is created, implementing `NavKey` and marked as `@Serializable`. This object will serve as the type-safe navigation destination for the details screen.

---
+++ feature/details/src/main/kotlin/com/example/hpoke/feature/details/navigation/DetailsNavigation.kt
@@ -0,0 +1,7 @@
+package com.example.hpoke.feature.details.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Details : NavKey
